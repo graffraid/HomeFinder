b52d6f20cc48eb8cdb8ee849e0939531935ea03d
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-07-13 14:52:40 +0300

    Initial commit

---
+++ Parser/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Parser
+{
+    using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Firefox;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://docs.seleniumhq.org/docs/03_webdriver.jsp#introducing-webdriver");
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
