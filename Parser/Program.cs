3f67ca9cb45d448b9ead35962af6fb62e79a98dc
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-07-13 18:44:41 +0300

    Add PhantomJS lib

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -3,12 +3,13 @@
     using System;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.PhantomJS;
 
     class Program
     {
         static void Main()
         {
-            using (IWebDriver driver = new FirefoxDriver())
+            using (IWebDriver driver = new PhantomJSDriver())
             {
                 driver.Navigate().GoToUrl("http://docs.seleniumhq.org/docs/03_webdriver.jsp#introducing-webdriver");
 
