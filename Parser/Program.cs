c91440434dc67934f95e204c33c52ee2fe5b2550
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-07-14 18:30:03 +0300

    Parser refactoring

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -12,26 +12,25 @@
     class Program
     {
         private static string url = "https://www.avito.ru/voronezh/kvartiry/prodam/vtorichka/kirpichnyy_dom?district=150&f=549_5698-5699-5700";
-        private static List<Building> buildingList;
-        private static readonly List<string> FilteredUrlList = new List<string>();
         
         static void Main()
         {
             var buildingRepository = new BuildingRepository();
-            buildingList = buildingRepository.GetAll();
+            List<Building> buildingList = buildingRepository.GetAll();
 
             using (IWebDriver driver = new PhantomJSDriver())
             //using (IWebDriver driver = new FirefoxDriver())
             {
-                StartParsing(driver);
+                List<string> urlList = GetAdvertUrls(driver, buildingList);
 
-                Console.WriteLine("====== The End! ======");
+                Console.WriteLine("\r====== Getting adverts finished. Count:{0} ======", urlList.Count);
                 Console.ReadKey(true);
             }
         }
 
-        private static void StartParsing(IWebDriver driver)
+        private static List<string> GetAdvertUrls(IWebDriver driver, List<Building> buildingList)
         {
+            List<string> result = new List<string>();
             driver.Navigate().GoToUrl(url);
 
             IList<IWebElement> pageElements = driver.FindElement(By.ClassName("pagination-pages")).FindElements(By.CssSelector("a.pagination-page"));
@@ -39,29 +38,32 @@
 
             foreach (var pageUrl in pageUrlList)
             {
-                Console.Write("\rParsing... page {0}/{1}", pageUrl.Key, pageUrlList.Count);
-                ParsePae(driver, pageUrl.Value);
+                Console.Write("\rGetting adverts... page {0}/{1}", pageUrl.Key, pageUrlList.Count);
+
+                IList<IWebElement> advertElements = ParsePage(driver, pageUrl.Value);
+                IList<IWebElement> filteredAdvertElements = FilterAdvertElements(advertElements, buildingList);
+                List<string> urlList = GetAdvertUrlsToList(filteredAdvertElements);
+                result.AddRange(urlList);
             }
+            return result;
         }
 
-        private static void ParsePae(IWebDriver driver, string url)
+        private static IList<IWebElement> ParsePage(IWebDriver driver, string url)
         {
             if (driver.Url != url)
             {
                 driver.Navigate().GoToUrl(url);
             }
-            IList<IWebElement> advertElements = driver.FindElements(By.ClassName("item"));
-            IList<IWebElement> filteredAdvertElements = FilterAdvertElements(advertElements);
-            AddAdvertUrlsToList(filteredAdvertElements);
+            return driver.FindElements(By.ClassName("item"));
         }
 
-        private static IList<IWebElement> FilterAdvertElements(IList<IWebElement> advertElements)
+        private static IList<IWebElement> FilterAdvertElements(IList<IWebElement> advertElements, List<Building> buildingList)
         {
             List<IWebElement> rezult = new List<IWebElement>();
 
             foreach (IWebElement element in advertElements)
             {
-                if (IsAdvertElementSatisfy(element.FindElement(By.ClassName("description")).FindElement(By.ClassName("address")).Text))
+                if (IsAdvertElementSatisfy(element.FindElement(By.ClassName("description")).FindElement(By.ClassName("address")).Text, buildingList))
                 {
                     rezult.Add(element);
                 }
@@ -69,12 +71,14 @@
             return rezult;
         }
 
-        private static void AddAdvertUrlsToList(IList<IWebElement> advertElements)
+        private static List<string> GetAdvertUrlsToList(IList<IWebElement> advertElements)
         {
+            List<string> result = new List<string>();
             foreach (IWebElement element in advertElements)
             {
-                FilteredUrlList.Add(element.FindElement(By.ClassName("description")).FindElement(By.ClassName("item-description-title")).FindElement(By.ClassName("item-description-title-link")).GetAttribute("href"));
+                result.Add(element.FindElement(By.ClassName("description")).FindElement(By.ClassName("item-description-title")).FindElement(By.ClassName("item-description-title-link")).GetAttribute("href"));
             }
+            return result;
         }
 
         private static Dictionary<int, string> GetPageUrls(IList<IWebElement> pageElements)
@@ -90,7 +94,7 @@
             return rezult;
         }
 
-        private static bool IsAdvertElementSatisfy(string elementAddr)
+        private static bool IsAdvertElementSatisfy(string elementAddr, List<Building> buildingList)
         {
             return buildingList.Any(addr => elementAddr.Contains(addr.ShortStreet) && elementAddr.Contains(addr.No));
         }
