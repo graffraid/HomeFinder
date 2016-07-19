92ad390ef1fbe87e0b4ea7e9d2af84aa6320f718
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-07-19 10:22:45 +0300

    Add web interface

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    using Domain.Repositories;
+
+    public class HomeController : Controller
+    {
+        private AdvertRepository advertRepository;
+
+        public HomeController()
+        {
+            advertRepository = new AdvertRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var adverts = advertRepository.GetAll();
+
+            return View(adverts);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
