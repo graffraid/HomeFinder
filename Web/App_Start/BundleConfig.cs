eea8140870b89a22e81d1ee002ba7e190e844a48
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-08-11 18:54:43 +0300

    Add yandex maps

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -18,7 +18,8 @@ namespace Web
                         "~/Scripts/jquery.signalR*"));
 
             bundles.Add(new ScriptBundle("~/bundles/custom").Include(
-                        "~/Scripts/foxyHubScript.js"));
+                        "~/Scripts/foxyHubScript.js",
+                        "~/Scripts/foxyMapScript.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
---
+++ Web/Scripts/foxyMapScript.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var myMap;
+    ymaps.ready(init);
+
+    function init() {
+        myMap = new ymaps.Map('map', {
+            center: [51.716730, 39.172982],
+            zoom: 16
+        });
+
+        myMap.controls.add('zoomControl', { left: 5, top: 5 });
+    }
+});
