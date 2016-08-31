f68e77fe4e118a99a8eccb74e716d1d4ebbd9578
Roman Ruzin <Roman.Ruzin@dataart.com>
2016-08-31 13:25:32 +0300

    Add elmah exception logger

--- Web/App_Start/WebApiConfig.cs
+++ Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿namespace Web
 {
     using System.Web.Http;
+    using System.Web.Http.ExceptionHandling;
+    using Elmah.Contrib.WebApi;
     using Newtonsoft.Json;
 
     public static class WebApiConfig
@@ -8,7 +10,7 @@
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
-
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
             var formatter = config.Formatters.JsonFormatter;
             formatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
         }
